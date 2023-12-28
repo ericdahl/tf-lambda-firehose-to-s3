438609222c286b783895bc5ad123bae157ace13e
Eric Dahl <eric.c.dahl@gmail.com>
2023-12-28 09:36:02 -0800

    add timestamp

--- lambda/main.py
+++ lambda/main.py
@@ -1,2 +1,4 @@
+import datetime
 def lambda_handler(event, context):
-    print("Hello World")
+    print(f"Hello world. Current Time is {datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')}")
+
